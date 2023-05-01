537c8b8d90cced04bba754ba9f441fe9fe831cfd
Dmitri <trainzment@yandex.com>
2023-05-01 23:15:10 +0400

    yn cmd ai

--- src/Commands/YnCmd.php
+++ src/Commands/YnCmd.php
@@ -5,6 +5,7 @@ namespace Revobot\Commands;
 use Revobot\Games\Predictor\Utils;
 use Revobot\Games\Predictor\YesNo;
 use Revobot\Revobot;
+use Revobot\Services\OpenAIService;
 
 class YnCmd extends BaseCmd
 {
@@ -33,6 +34,10 @@ class YnCmd extends BaseCmd
 
         $input = Utils::replaceDate($this->input);
         $input = Utils::replaceMe($this->bot->getUserId(), $input);
+
+        if (USE_AI_CMD) {
+            return OpenAIService::generate($input, "На все вопросы и предложения отвечай только да или нет. Если нельзя ответить или оскорбительно, то пиши не знаю", []);
+        }
         return (new YesNo($input))->calc();
     }
 

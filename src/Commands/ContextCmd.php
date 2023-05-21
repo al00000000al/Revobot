0ca61643afcac020549da6d3308cbf57cd40fb3f
Dmitri <trainzment@yandex.com>
2023-05-21 13:37:59 +0400

    context view

--- src/Commands/ContextCmd.php
+++ src/Commands/ContextCmd.php
@@ -2,17 +2,15 @@
 
 namespace Revobot\Commands;
 
+use Revobot\Games\AI\GptPMC;
 use Revobot\Revobot;
 
 class ContextCmd extends BaseCmd
 {
     private Revobot $bot;
-
     public const KEYS = ['context','контекст'];
     public const IS_ENABLED = true;
     public const HELP_DESCRIPTION = 'Установить контекст нейросети';
-
-    private const PMC_USER_AI_KEY = 'pmc_user_ai_';
 
     public function __construct(string $input, Revobot $bot)
     {
@@ -23,21 +21,13 @@ class ContextCmd extends BaseCmd
 
     public function exec(): string
     {
+        $GptPMC = new GptPMC($this->bot->pmc, $this->bot->getUserId(), $this->bot->provider);
+
         if (!empty($this->input)) {
-            $this->setContext($this->input);
+            $GptPMC->setContext($this->input);
             return "Значение изменено";
         }
-        $this->setContext("");
-        return $this->description;
+        return $GptPMC->getContext();
     }
 
-
-    private function setContext(string $context){
-        $this->bot->pmc->set($this->getKey(), $context);
-    }
-
-
-    private function getKey(){
-        return self::PMC_USER_AI_KEY . $this->bot->provider . $this->bot->getUserId();
-}
 }

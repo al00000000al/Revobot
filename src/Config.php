536cc7c4c8e60eb0c68d26e3f7a3129b6483ee4e
Dmitri <trainzment@yandex.com>
2023-05-24 00:21:04 +0400

    config fix

--- src/Config.php
+++ src/Config.php
@@ -5,12 +5,12 @@ namespace Revobot;
 class Config {
     public static function get(string $key) {
         global $config;
-        return ($config[$key] ?? $key);
+        return (string) ($config[$key] ?? $key);
     }
 
     public static function getArr(string $key) {
         global $config;
-        return $config[$key] ?? [];
+        return (array)$config[$key] ?? [];
     }
 
     public static function getInt(string $key) {

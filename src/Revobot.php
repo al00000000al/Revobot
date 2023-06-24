0d2f0bff504fdad4cf3fe99db240fc5d87bdf86f
Dmitri <trainzment@yandex.com>
2023-06-25 00:15:14 +0400

    fix name

--- src/Revobot.php
+++ src/Revobot.php
@@ -183,7 +183,7 @@ class Revobot
                 $source_text = (string)$this->raw_data['reply_to_message']['text'];
                 $from_id = (int)$this->raw_data['reply_to_message']['from']['id'];
                 if($from_id === Config::getInt('tg_bot_id') && !empty($source_text)) {
-                    $this->bot->sendTypeStatusTg();
+                    $this->sendTypeStatusTg();
                     $this->sendMessageTg(Gpt::generate($this->message, $this->getUserId(), $this->provider));
                 }
             }

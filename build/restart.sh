11f9e165f32180a8812652098d1dacbf6373020f
Dmitri <trainzment@yandex.com>
2023-12-20 22:22:55 +0400

    restart.sh

--- build/restart.sh
+++ build/restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pkill -f revobot
-nohup ../kphp_out/server -H 8088 --user kitten --use-utf8 --workers-num 5 -q   >/dev/null 2>&1 &
+cd ../
+nohup ./kphp_out/server -H 8088 --user kitten --use-utf8 --workers-num 5 -q   >/dev/null 2>&1 &

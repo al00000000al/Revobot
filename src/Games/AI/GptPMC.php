b118b9e3395993a3d3e3fc1686c30410d79c50c1
Dmitri <trainzment@yandex.com>
2023-12-20 21:53:04 +0400

    PMC class fixes

--- src/Games/AI/GptPMC.php
+++ src/Games/AI/GptPMC.php
@@ -2,6 +2,8 @@
 
 namespace Revobot\Games\AI;
 
+use Revobot\Util\PMC;
+
 class GptPMC
 {
     private const PMC_USER_AI_KEY = 'pmc_user_ai_';
@@ -11,51 +13,55 @@ class GptPMC
     private int $user_id;
     private string $provider;
 
-    /** @var  \Memcache $pmc */
-    private $pmc;
-
-    public function __construct(int $user_id, string $provider = 'tg') {
-        global $pmc;
-        $this->pmc = $pmc;
+    public function __construct(int $user_id, string $provider = 'tg')
+    {
         $this->user_id = $user_id;
         $this->provider = $provider;
     }
 
-    public function getContext() {
+    public function getContext()
+    {
 
-        return (string) $this->pmc->get(self::getContextKey($this->user_id, $this->provider));
+        return (string) PMC::get(self::getContextKey($this->user_id, $this->provider));
     }
 
-    public function setContext(string $context) {
-        $this->pmc->set(self::getContextKey($this->user_id, $this->provider), $context);
+    public function setContext(string $context)
+    {
+        PMC::set(self::getContextKey($this->user_id, $this->provider), $context);
     }
 
-    public function getHistory() {
-        return (array) json_decode($this->pmc->get(self::getHistoryKey($this->user_id, $this->provider)), true);
+    public function getHistory()
+    {
+        return (array) json_decode(PMC::get(self::getHistoryKey($this->user_id, $this->provider)), true);
     }
 
-    public function setHistory(array $history) {
-        $this->pmc->set(self::getHistoryKey($this->user_id, $this->provider),  json_encode($history));
+    public function setHistory(array $history)
+    {
+        PMC::set(self::getHistoryKey($this->user_id, $this->provider),  json_encode($history));
     }
 
-    public function setInput(string $message) {
-        $this->pmc->set(self::getInputKey($this->user_id, $this->provider),  ($message));
+    public function setInput(string $message)
+    {
+        PMC::set(self::getInputKey($this->user_id, $this->provider), ($message));
     }
 
-    public function deleteHistory() {
-        $this->pmc->delete(self::getHistoryKey($this->user_id, $this->provider));
+    public function deleteHistory()
+    {
+        PMC::delete(self::getHistoryKey($this->user_id, $this->provider));
     }
 
-    private static function getContextKey(int $user_id, string $provider) {
+    private static function getContextKey(int $user_id, string $provider)
+    {
         return self::PMC_USER_AI_KEY . $provider . $user_id;
     }
 
-    private static function getHistoryKey(int $user_id, string $provider) {
+    private static function getHistoryKey(int $user_id, string $provider)
+    {
         return self::PMC_USER_AI_HISTORY_KEY . $provider . $user_id;
     }
 
-    public static function getInputKey(int $user_id, string $provider) {
+    public static function getInputKey(int $user_id, string $provider)
+    {
         return self::PMC_USER_AI_INPUT_KEY . $provider . $user_id;
     }
-
 }
--- src/Util/PMC.php
+++ src/Util/PMC.php
@@ -4,15 +4,21 @@ namespace Revobot\Util;
 
 class PMC
 {
+    /** @var PMC|null */
     private static $instance = null;
+
+    /** @var \McMemcache */
     private $memcache;
 
     private function __construct()
     {
-        $this->memcache = new \Memcache();
-        $this->memcache->connect('127.0.0.1', 11209);
+        $this->memcache = new \McMemcache();
+        $this->memcache->addServer('127.0.0.1', 11209);
     }
 
+    /**
+     * @return \McMemcache
+     */
     public static function getInstance()
     {
         if (self::$instance === null) {
@@ -21,21 +27,38 @@ class PMC
         return self::$instance->memcache;
     }
 
+    /**
+     * @param string $key
+     * @return mixed
+     */
     public static function get($key)
     {
         return self::getInstance()->get($key);
     }
 
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param int $expiration
+     * @return bool
+     */
     public static function set($key, $value, $expiration = 0)
     {
         return self::getInstance()->set($key, $value, 0, $expiration);
     }
 
+    /**
+     * @param string $key
+     * @return bool
+     */
     public static function delete($key)
     {
         return self::getInstance()->delete($key);
     }
 
+    /**
+     * @return mixed
+     */
     public static function getVersion()
     {
         return self::getInstance()->getVersion();

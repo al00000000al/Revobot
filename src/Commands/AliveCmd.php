320eb1f2cfd7693486ed37ee1fd557eef8a22692
Dmitri <trainzment@yandex.com>
2023-12-20 23:09:52 +0400

    gen

--- src/Commands/AliveCmd.php
+++ src/Commands/AliveCmd.php
@@ -23,6 +23,6 @@
          */
         public function exec(): string {
             $pmc_v = PMC::getVersion();
-            return "Жив! PMC: $pmc_v, Bot build: 172";
+            return "Жив! PMC: $pmc_v, Bot build: 173";
         }
     }

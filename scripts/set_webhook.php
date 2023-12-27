22234d924eaefc77f4c7b030b88ae4c45dca734e
Dmitri <trainzment@yandex.com>
2023-12-27 05:31:54 +0400

    fix

--- scripts/set_webhook.php
+++ scripts/set_webhook.php
@@ -1,12 +1,12 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config.php';
 
 use Revobot\Config;
 use Revobot\Services\Providers\Tg;
 
-require __DIR__ . '/../config.php';
 
-Tg::setWebhook(Config::get('telegram_webhook_url'));
-
+$url = Config::get('telegram_webhook_url');
+Tg::setWebhook($url);
 echo "Webhook {$url} установлен!\r\n";

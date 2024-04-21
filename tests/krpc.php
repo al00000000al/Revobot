00c5e1d4c3f58a9441ee583011bd4e249792363a
Dmitri <trainzment@yandex.com>
2024-04-21 21:06:49 +0400

    rpc fix

--- tests/krpc.php
+++ tests/krpc.php
@@ -1,14 +1,16 @@
 <?php
 
-$conn = new_rpc_connection('127.0.0.1', 11209);
+$conn = new_rpc_connection('127.0.0.1', 11209, 0, 0);
 $user_id = 198239789;
 $query_ids = rpc_tl_query(
     $conn,
     [
         ['memcache.get', 'user_timezone_tg' . $user_id],
         ['memcache.get', 'tg_username' . $user_id],
-        ['memcache.get', 'money_tg' . $user_id]
-    ]
+        ['memcache.get', 'money_tg' . $user_id],
+        ['memcache.get', 'rndempty'],
+    ],
+    0
 );
 var_dump($query_ids);
 $response = rpc_tl_query_result($query_ids);

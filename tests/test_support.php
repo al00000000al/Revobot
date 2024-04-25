e26e1700a72783ef639d6601f22a6e279ae27806
Dmitri <trainzment@yandex.com>
2024-04-26 00:59:41 +0400

    не работает

--- tests/test_support.php
+++ tests/test_support.php
@@ -4,18 +4,24 @@ $hl->addServer('127.0.0.1', 11247);
 $question_id = 1; //ID вопроса. Не обязательно
 $user_id = 1; //ID юзера. Не обязательно
 $mark = 5; //Оценка
-$question  = "Кто проживает на дне океана?"; //вопрос
-$answer = "Губка боб кводратные штоны!"; //Ответ
+$question  = "Где найти Content Manager ?"; //вопрос
+
+// $answer = "Спам для пидоров"; //Ответ
 
 $question = iconv('UTF-8', 'WINDOWS-1251', $question);
-$answer = iconv('UTF-8', 'WINDOWS-1251', $answer);
+// $answer = iconv('UTF-8', 'WINDOWS-1251', $answer);
 
 // var_dump($hl->getStats());
 
 // $hl->set("answer{$question_id},{$user_id},{$mark}", $question . "\t" . $answer);
 
-// $random_tag = mt_rand(111, 999);
-var_dump($hl->set("question1",  "Кто проживает на дне океана?"));
-var_dump($hl->get("answer1,1,1[#30]"));
+$random_tag = mt_rand(111, 999);
+$hl->set("question1",  $question);
+$result = $hl->get("answer1,1,1#30");
+print_r($result);
+if ($result) {
+    $answ = $result[0][1];
+    echo $answ . PHP_EOL;
+}
 
 // $hl->get("delete_answer{$question_id}");

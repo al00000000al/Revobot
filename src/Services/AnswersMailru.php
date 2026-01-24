1863ccc2bfb6c2e40e32c20be27df64dd335f2c1
Dmitri <trainzment@yandex.com>
2026-01-24 22:02:02 +0800

    update AnswersMailru

--- src/Services/AnswersMailru.php
+++ src/Services/AnswersMailru.php
@@ -8,7 +8,7 @@ class AnswersMailru
 {
 
     private const BASE_GO_URL = 'https://go.mail.ru/answer_json?';
-    private const BASE_OTVET_URL = 'https://otvet.mail.ru/api/v2/question?';
+    private const BASE_OTVET_URL = 'https://otvet.mail.ru/api/topic/answers/';
 
 
     /**
@@ -18,44 +18,42 @@ class AnswersMailru
     public static function get(string $input): string
     {
 
-        $params = http_build_query([
-            'ajax_id' => 5,
-            'num' => 1,
-            'sf' => 0,
-            'zvstate' => 1,
-            'q' => $input
-        ]);
-
-        $results = Curl::get(self::BASE_GO_URL . $params);
-
-
-        $questions = (array)json_decode($results, true);
+        $questions = self::searchQuestions($input);
 
         if (!isset($questions['results']) || count($questions['results']) === 0) {
             return '';
         }
 
-        // $random_qid = (int) $questions['results'][mt_rand(0, count($questions['results']) - 1)]['id'];
-        if (count($questions['results']) > 3) {
-            $cnt = 2;
-        } else {
-            $cnt = count($questions['results']);
-        }
-        $random_qid = (int) $questions['results'][mt_rand(0, $cnt)]['id'];
+        $random_qid = (int) $questions['results'][mt_rand(0, count($questions['results']))]['id'];
 
-        $params = http_build_query(['qid' =>  $random_qid]);
-
-        $answers = Curl::get(self::BASE_OTVET_URL . $params);
-
-        $answers = (array)json_decode($answers, true);
-
-
-        // if (!isset($answers['bestanswer'])) {
-        //     return '';
-        // }
-        //$bestanswer = (int)$answers['bestanswer'];
-
-        $text = $answers['answers'][mt_rand(0, count($answers['answers']) - 1)]['atext'];
+        $answers = self::getAnswers($random_qid);
+        $text = self::getRandomAnswerText($answers);
         return (string)strip_tags($text);
+    }
+
+    public static function searchQuestions(string $text): array
+    {
+        $results = (string) Curl::get(self::BASE_GO_URL . http_build_query([
+            'ajax_id' => 5,
+            'num' => 1,
+            'sf' => 0,
+            'zvstate' => 1,
+            'q' => $text
+        ]));
+        return (array)json_decode($results, true);
+    }
+
+    public static function getAnswers(int $qid): array
+    {
+        $answers = Curl::get(self::BASE_OTVET_URL . $qid . '?limit=1000&dir=0&sort=rating&order_by=desc');
+        return (array)json_decode($answers, true);
+    }
+
+    public static function getRandomAnswerText(array $answers): string
+    {
+        $replies_cnt = count($answers['result']['replies']);
+
+        $text = $answers['result']['replies'][mt_rand(0, $replies_cnt) - 1]['content']['content'][0]['content'][0]['text'];
+        return (string)$text;
     }
 }
